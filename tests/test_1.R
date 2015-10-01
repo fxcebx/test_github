87906d914c7bebe625ac39ed7bd3a481fbf32a57
fxcebx <fx.cebx@gmx.at>
2015-10-01 23:53:37 +0200

    add testthat in /test/test_1.R

---
+++ tests/test_1.R
@@ -0,0 +1,12 @@
+# Example Unit Testing Script
+
+expect_that(1, equals(1))
+
+# Check that the source data has the expected format
+
+# test_that("data has loaded as expected", {
+#   expect_that(length(local.reviews),              equals(5))
+#   expect_that(length(outdoor.furniture.assets),   equals(8))
+#   expect_that(licenced.premises$LicenceNo,        is_a("integer"))
+#   expect_that(location.info$zip,                  is_a("character"))
+# })
